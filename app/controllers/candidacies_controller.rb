ac2c7217b6ab49126d7a70b62b9a462af0cd0154
remysichel <remysichel@gmail.com>
2019-01-07 11:56:03 +0100

    candidaies index show

--- app/controllers/candidacies_controller.rb
+++ app/controllers/candidacies_controller.rb
@@ -1,2 +1,11 @@
 class CandidaciesController < ApplicationController
+  skip_before_action :authenticate_company!
+
+  def index
+    @candidacies = Candidacy.where(jobseeker_id: current_jobseeker.id)
+  end
+
+  def show
+    @candidacy = Candidacy.find(params[:id])
+  end
 end
